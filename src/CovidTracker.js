787efc3b129a7dbe9036e926bd4758ef2bdf4810
Raz <razrimon27@gmail.com>
2020-09-13 19:18:03 +0300

    Fixed display delay

--- src/CovidTracker.js
+++ src/CovidTracker.js
@@ -1,26 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import * as R from 'ramda';
+//import {Bar, Line, Pie} from 'chart'
 
 export default function CovidTracker(){
     const [dateList, setDateList] = useState([]);
     const [startDate, setStartDate] = useState('2020-09-04');
     const [endDate, setEndDate] = useState('2020-09-09');
     const [countries, setCountries] = useState([]);
-    const [country, setCountry] = useState('');
+    const [country, setCountry] = useState('ABW');
     const [countryImage, setCountryImage] = useState('');
     const [countryName, setCountryName] = useState([]);
     const [showContent, setShowContent] = useState(false);
 
-        useEffect(() => {
-            axios.get(`https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/${startDate}/${endDate}`)
-            .then((res) => { 
-                setCountries(res.data.data[startDate])
-            })
-        },[])
+    useEffect(() => {
+        axios.get(`https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/2020-09-04/2020-09-04`)
+        .then((res) => {
+            setCountries(res.data.data['2020-09-04'])
+        })
+    },[])
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Access-Control-Allow-Origin': 'http://localhost:3000',
+            'Access-Control-Allow-Credentials': 'true'
+        }
+    }
 
     const countryPicked = () => {
-        axios.get(`https://cors-anywhere.herokuapp.com/http://countryapi.gear.host/v1/Country/getCountries?pAlpha3Code=${country}`)
+        axios.get(`https://cors-anywhere.herokuapp.com/http://countryapi.gear.host/v1/Country/getCountries?pAlpha3Code=${country}`, config)
         .then((countryRes) => {
             console.log("Name: ",countryRes.data.Response[0].Name)
             console.log("Flag: ",countryRes.data.Response[0].Flag)
@@ -31,12 +41,14 @@ export default function CovidTracker(){
 
     const submit = (startDate, endDate) => {
         countryPicked();
+        setCountryImage('');
+        setCountryName('');
         console.log("startDate: "+startDate);
         console.log("endDate: "+endDate);
         axios.get(`https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/${startDate}/${endDate}`)
         .then((res) => { 
             setDateList(res.data.data)
-            })
+        })
         setShowContent(true);
     }
 
@@ -66,7 +78,7 @@ export default function CovidTracker(){
 
 export function List(props){
     return(
-        props.showContent && <div>
+        props.showContent && props.country && <div>
             <h3>{props.countryName}</h3>
             <img src={props.countryImage} width="200px"></img>
             {props.dateList.length !== 0 && Object.entries(props.dateList).map(([key,value]) => {
