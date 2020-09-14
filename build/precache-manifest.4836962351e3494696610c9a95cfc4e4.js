c559d98960467f5a9547227c5059b383ff51bde3
Raz <razrimon27@gmail.com>
2020-09-14 12:39:18 +0300

    Modified gitignore

---
+++ build/precache-manifest.4836962351e3494696610c9a95cfc4e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87f019b5339df4f541c65f06d9227b8",
+    "url": "/covertracker/index.html"
+  },
+  {
+    "revision": "377bd9d692ec7ee39282",
+    "url": "/covertracker/static/css/main.1c407655.chunk.css"
+  },
+  {
+    "revision": "69e11ac35e315d48e0da",
+    "url": "/covertracker/static/js/2.5aafb79c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covertracker/static/js/2.5aafb79c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377bd9d692ec7ee39282",
+    "url": "/covertracker/static/js/main.2b957954.chunk.js"
+  },
+  {
+    "revision": "a96355fa207218f7cdef",
+    "url": "/covertracker/static/js/runtime-main.e828a481.js"
+  }
+]);
