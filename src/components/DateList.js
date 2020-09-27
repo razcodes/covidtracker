e379313ae174aac3d363a42bfcc24ee8ad3b6752
Raz <razrimon27@gmail.com>
2020-09-27 16:57:33 +0300

    Fixed daily infected logic

--- src/components/DateList.js
+++ src/components/DateList.js
@@ -24,62 +24,50 @@ export default function DateList(props){
         calcDailyConfirmed(data);
     }
 
-    const calcDailyConfirmed = (data) => {
+    const calcDailyConfirmed = (data) => {        
+
+        // Creating an array made of confirmed infected
         const confirmedArray = [];
-
-        data.map((day, i) => {})
-
-        const newConfirmed = data.map((day, i) => {
-            if(day === undefined){
-                dailyArray.push(null)
+        data.map((day, i) => {
+            if(day !== undefined && data[i+1] !== undefined){
+                confirmedArray.push(day.data.confirmed)
                 return;
             }
 
+            confirmedArray.push(null)
+            return;
+
+        })
+        console.log("confirmedArray: ", confirmedArray)
+
+        // Creating an array based on the confirmed one with daily infected
+        const newArr = [null];
+        confirmedArray.map((num, i)=>{
+            if(num!==null){
+                let daily = confirmedArray[i+1]-num;
+                newArr[i]=daily;
+            }
+            else{
+                newArr[i]=null;
+            }
+        })
+        console.log("newArr: ", newArr)
+
+        const newConfirmed = data.map((day, i) => {
             if(day !== undefined && data[i+1] !== undefined){
-                const newSick = data[i+1].data.confirmed ? data[i+1].data.confirmed-day.data.confirmed : data[i+1].data;
-                dailyArray.push(newSick)
                 return ({
                     id: day.id,
                     date_value: day.date_value,
                     data: {
                         confirmed: day.data.confirmed,
                         deaths: day.data.deaths,
-                        daily: dailyArray[i]
-                        //daily: data[i+1].data.confirmed !== undefined ? data[i+1].data.confirmed-day.data.confirmed : 0
+                        daily: newArr[i-1]
                     }
                 })
             }
         })
         setCountryData(newConfirmed);
-        console.log("dailyArray: ", dailyArray);
     }
-    // const calcDailyConfirmed = (data) => {
-    //     const dailyArray = [null];
-
-    //     const newConfirmed = data.map((day, i) => {
-    //         if(day === undefined){
-    //             dailyArray.push(null)
-    //             return;
-    //         }
-
-    //         if(day !== undefined && data[i+1] !== undefined){
-    //             const newSick = data[i+1].data.confirmed ? data[i+1].data.confirmed-day.data.confirmed : data[i+1].data;
-    //             dailyArray.push(newSick)
-    //             return ({
-    //                 id: day.id,
-    //                 date_value: day.date_value,
-    //                 data: {
-    //                     confirmed: day.data.confirmed,
-    //                     deaths: day.data.deaths,
-    //                     daily: dailyArray[i]
-    //                     //daily: data[i+1].data.confirmed !== undefined ? data[i+1].data.confirmed-day.data.confirmed : 0
-    //                 }
-    //             })
-    //         }
-    //     })
-    //     setCountryData(newConfirmed);
-    //     console.log("dailyArray: ", dailyArray);
-    // }
 
     const CountryDataHTML = () => (
         Object.entries(countryData).map(([key, value], i)=>(
@@ -115,20 +103,3 @@ export default function DateList(props){
             </div>
     )   
 }
-
-
-    // const CountryDataHTML = () => (
-    //     Object.entries(props.dates).map(([key, value], i) => ( // key = dates, value = a3alpha codes
-    //         <div key={i} className='date-box'>
-    //             {!value[props.countryCode] && <div className="date-text">
-    //                     <div><b>No data for this date</b></div>
-    //                 </div>}
-    //             {value[props.countryCode] && <div className="date-text">
-    //                     {value[props.countryCode].date_value && <div><b>{formatDate(value[props.countryCode].date_value)}</b></div>}
-    //                     {value[props.countryCode].confirmed && <div>Confirmed: {value[props.countryCode].confirmed.toLocaleString()}</div>}
-    //                     {value[props.countryCode].deaths && <div>Deaths: {value[props.countryCode].deaths.toLocaleString()}</div>}
-    //                 </div>}
-    //         </div>
-    //     ))
-    // )
-
