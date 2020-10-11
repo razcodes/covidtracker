76d3a6db8b1b53c2376ac905d5fb967497242b81
Raz <razrimon27@gmail.com>
2020-10-11 19:03:05 +0300

    Updates

---
+++ precache-manifest.c32384b4077b37b4e7bcf8a850d386d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce207c41919a129a4041d5b92cc1987",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "f3c57745f772f78f69f4",
+    "url": "/covidtracker/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "12422b890f2b2989b2e6",
+    "url": "/covidtracker/static/css/main.c62ba755.chunk.css"
+  },
+  {
+    "revision": "f3c57745f772f78f69f4",
+    "url": "/covidtracker/static/js/2.ccd36010.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/covidtracker/static/js/2.ccd36010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12422b890f2b2989b2e6",
+    "url": "/covidtracker/static/js/main.dc900269.chunk.js"
+  },
+  {
+    "revision": "abb288adae797851ddc5",
+    "url": "/covidtracker/static/js/runtime-main.345c4fa2.js"
+  }
+]);
